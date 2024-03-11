1ce010fe73bd7b54b08f7765a235873fa8d490a9
Pavel Stepanov <impetus@yandex.ru>
2024-03-11 11:28:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String image_names[] = {
+            "Clouds",
+            "Sun",
+            "Partial clouds",
+            "Snow",
+            "Sleet",
+            "Mist",
+            "Clear",
+            "Rain",
+            "Rain thunder",
+            "Fog"
+    };
+
+    private final String image_urls[] = {
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/angry_clouds.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/day_clear.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_half_moon_partial_cloud.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_half_moon_snow.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/sleet.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/mist.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_full_moon_clear.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_half_moon_rain.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/rain_thunder.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/fog.png"
+    };
+
+    class Item {
+        private String title;
+        private String url;
+
+        public Item(String title, String url) {
+            this.title = title;
+            this.url = url;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+    private ArrayList<Item> generateList() {
+        ArrayList<Item> list = new ArrayList<>();
+        for(int i=0;i<image_names.length;i++) {
+            list.add(new Item(image_names[i],image_urls[i]));
+        }
+        return list;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(generateList()));
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
